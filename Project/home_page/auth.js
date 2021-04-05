454d00ecb8b6e8016e649ec6f20cce3d51d8aa14
ampeo <ampeo@yandex.ru>
2021-04-05 04:12:19 +0300

    auth and create profile page

--- Project/home_page/auth.js
+++ Project/home_page/auth.js
@@ -1,38 +1,23 @@
-    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        console.log("пользователь авторизован")
+        console.log(user.email)
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/firebase.User
+      var uid = user.uid;
+      // ...
+    } else {
+        console.log("пользователь не авторизован")
+      // User is signed out
+      // ...
+    }
+});
 
-    var uiConfig = {
-    callbacks: {
-        signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-            console.log("вошёл");
-            return true;
-        },
-        uiShown: function() {
-            // The widget is rendered.
-            // Hide the loader.
-            console.log("вошёл");
-            document.getElementById('loader').style.display = 'none';
-        }
-    },
-    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-    signInFlow: 'popup',
-    signInSuccessUrl: 'http://127.0.0.1:5500/home_page/home_page.html',
-    signInOptions: [
-        firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    ],
-    // Terms of service url.
-        tosUrl: '<your-tos-url>',
-    // Privacy policy url.
-        privacyPolicyUrl: '<your-privacy-policy-url>'
-    };
+    let errorMail = "";
+    let errorPassword = "";
 
     document.querySelector('#showRegisterDialog').addEventListener("click", showRegisterDialog);
     function showRegisterDialog(){
-        /*ui.start('#firebaseui-auth-container', {
-            signInOptions: [
-              firebase.auth.EmailAuthProvider.PROVIDER_ID
-            ],
-            // Other config options...
-        });*/
         let dialog = document.querySelector('#registerDialog');
         dialog.showModal()
     }
@@ -45,7 +30,6 @@
 
     document.querySelector('#showLogInDialog').addEventListener("click", showLogInDialog);
     function showLogInDialog(){
-        //ui.start('#firebaseui-auth-container', uiConfig);
         let dialog = document.querySelector('#logInDialog');
         dialog.showModal()
     }
@@ -64,37 +48,40 @@
         var dateOfBirth = this.querySelector("input[name=dateOfBirth]").value;
 
         firebase.auth().createUserWithEmailAndPassword(mail, password).then((userCredential) => {
-            // Signed in 
             var user = userCredential.user;
             user.displayName = username;
-            console.log("Пользователь зарегистрирован.");
-            // ...
         })
-        .catch((error) => {
-            var errorCode = error.code;
-            console.log(errorCode)
-            var errorMessage = error.message;
-            console.log(errorMessage)
-        });
+        .catch((error) => console.log(error));
 
     }
 
     document.getElementById('register').onsubmit = register;
 
-    function logIn(){
-        var mail = this.querySelector("input[name=mail]").value;
-        var password = this.querySelector("input[name=password]").value;
-        document.location.href = 'profile_page.html'
-        firebase.auth().signInWithEmailAndPassword(mail, password).then((userCredential) => {
-            // Signed in
+    function logIn(event){
+        event.preventDefault();
+
+        var email = this.querySelector("input[name=mail]");
+        var password = this.querySelector("input[name=password]");
+
+        firebase.auth().signInWithEmailAndPassword(email.value, password.value)
+        .then((userCredential) => {
             var user = userCredential.user;
-            document.path = '/home_page/profile_page.html'
-            // ...
+            document.location = "/home_page/profile_page.html"
         })
         .catch((error) => {
             var errorCode = error.code;
+            console.log(errorCode);
             var errorMessage = error.message;
+            console.log(errorMessage);
+            event.stopPropagation
         });
     }
 
-    document.getElementById('logIn').onsubmit = logIn;
+    document.getElementById('logIn').addEventListener('submit', logIn)
+
+    document.querySelector('#signOut').addEventListener("click", signOut);
+    function signOut(){
+        if (firebase.auth().currentUser != null){
+            firebase.auth().signOut();
+        }
+    }
